e3ea829d0dc57f7d7931e531d67160d5bf2c4a65
Josefin Werme <josefinwerme@gmail.com>
2021-01-08 18:15:42 +0100

    updated run.univ.bivar()

--- R/analysis_functions.R
+++ R/analysis_functions.R
@@ -35,7 +35,7 @@ run.univ.bivar = function(locus, phenos=NULL, target=NULL, univ.thresh=.05, adap
 	bivar = NULL
 	if (sum(univ$p < univ.thresh) > 1) {
 		if (!is.null(target)) { if (subset(univ, phen==target)$p > univ.thresh) { break() } }	# if target is specified, only proceed if target is sig
-		bivar = run.bivar(locus, phenos = subset(univ, p < univ.thresh)$phen, target=target, adap.thresh=adap.thresh, p.values=p.values, CIs=CIs, param.lim=param.lim)
+		bivar = run.bivar(locus, phenos = as.character(subset(univ, p < univ.thresh)$phen), target=target, adap.thresh=adap.thresh, p.values=p.values, CIs=CIs, param.lim=param.lim)
 	}
 	return(list(univ=univ, bivar=bivar))
 }
