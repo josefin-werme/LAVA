2f25866dacc17c90fb2cf3970c9f2264a77a177d
cadeleeuw <76732514+cadeleeuw@users.noreply.github.com>
2022-04-17 18:56:07 +0200

    Update input_eqtl.r

--- R/input_eqtl.r
+++ R/input_eqtl.r
@@ -1,7 +1,7 @@
 #' Process eQTL input
 #'
 #' Modifies the GWAS input object, appending eQTL (or sQTL) summary statistics and gene annotation to an existing GWAS input. See \code\link[LAVA website]{https://ctg.cncr.nl/software/lava}
-#' for preformatted GTEx v8 input files and details on the input file format. 
+#' for preformatted GTEx v8 input files and details on the formatting of the input files.
 #'
 #' @param gwas.input GWAS input object generated using \code{\link{process.input}} (object is modified by function).
 #'
